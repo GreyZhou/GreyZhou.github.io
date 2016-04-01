e7867ab97ed77bddee46fbb668df5228a402c41e
greyzhou <251198349@qq.com>
2016-04-01 17:36:36 +0800

    01-07

---
+++ ife/01/task7/main.js
@@ -0,0 +1,15 @@
+// window.onload=function(){
+// 	var fo=document.getElementById('tou').getElementsByTagName("li");
+// 	for (var i = 0; i < fo.length; i++) {
+// 		var a=document.getElementsByClassName("focus");
+// 		(function(i){
+// 			fo[i].onmouseover=function(){
+// 				if (this!=a) {
+// 					a.className="";
+// 					this.className="focus";
+// 				}	
+// 			}
+// 		})(i);
+// 	}
+
+// }
